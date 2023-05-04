de8f1cde804f8f8703b1d9b84deeeee938306038
Grigory <lololohka057@gmail.com>
2023-05-04 17:36:20 +0300

    finish

--- utils.py
+++ utils.py
@@ -21,28 +21,44 @@ def return_data(data):
 
 data_sorted = return_data(data)
 
-def view_screem(data_sorted):
+def description(data_sorted, y):
    text = []
-   y = 0
-   while True:
-      text += data_sorted[y]["date"][0:10], data_sorted[y]["description"]
-      print(data_sorted[y]["date"][0:10], data_sorted[y]["description"])
+   text += data_sorted[y]["date"][0:10], data_sorted[y]["description"]
+   print(data_sorted[y]["date"][0:10], data_sorted[y]["description"])
+   return text
       
-      if data_sorted[y]["description"] == "Открытие вклада":
-         text += data_sorted[y]["to"]
-         print(data_sorted[y]["to"])
-      
-      if data_sorted[y]["description"] != "Открытие вклада":
+def screen(data_sorted,y):
+   text = []
+   if data_sorted[y]["description"] != "Открытие вклада":
          from_ = data_sorted[y]["from"].split(" ")
          to_ = data_sorted[y]["to"].split(" ")
          text += from_[-2] , from_[-1][0:4] , f"{from_[-1][4:6]}**" , "****" , from_[-1][-4:] , "-->" , to_[-2] , f"**{to_[-1][16:]}"
          print(from_[-2] , from_[-1][0:4] , f"{from_[-1][4:6]}**" , "****" , from_[-1][-4:] , "-->" , to_[-2] , f"**{to_[-1][16:]}")
+   return text
 
-      text += data_sorted[y]["operationAmount"]["amount"] , data_sorted[y]["operationAmount"]["currency"]["name"]
-      print(data_sorted[y]["operationAmount"]["amount"] , data_sorted[y]["operationAmount"]["currency"]["name"])
+def amount(data_sorted,y):
+   text = []
+   text += data_sorted[y]["operationAmount"]["amount"] , data_sorted[y]["operationAmount"]["currency"]["name"]
+   print(data_sorted[y]["operationAmount"]["amount"] , data_sorted[y]["operationAmount"]["currency"]["name"])
+   return text
 
-      text += ""
-      print("")
+def space():
+   text = []
+   text += ""
+   print("")
+   return text
+
+def view_screem(data_sorted):
+   text = []
+   y = 0
+   while True:
+      text += description(data_sorted,y)
+      
+      text += screen(data_sorted,y)
+
+      text += amount(data_sorted,y)
+
+      text += space()
       y += 1
       
       if y == 5:
@@ -50,4 +66,3 @@ def view_screem(data_sorted):
    return text
 
 
-text = view_screem(data_sorted)
